119c8d4504036b6bed0825e90bda220713913638
tulinmola <tulinmola@gmail.com>
2017-11-30 14:54:21 +0100

    Add elixir scaffolding

---
+++ lib/logrex.ex
@@ -0,0 +1,18 @@
+defmodule Logrex do
+  @moduledoc """
+  Documentation for Logrex.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Logrex.hello
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
